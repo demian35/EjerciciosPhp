a3929162f303bd3b868236e17ec20719648db978
Demian <53890358+demian35@users.noreply.github.com>
2023-06-27 22:03:24 -0600

    ejemplos de variables y constantes en php

---
+++ Ejercicio7ConstantesyVariables/constantes_variables.php
@@ -0,0 +1,23 @@
+<?php
+
+$nombre="Jose"; //valor variable
+
+$edad=26; // valor variable
+
+echo "Nombre: "."<br>" . $nombre." <br>". " Edad: "."<br>". $edad."<br>";
+
+$nombre="Demian";
+$edad=30; 
+
+echo "Nombre: "."<br>" . $nombre." <br>". " Edad: "."<br>". $edad."<br>";
+
+//delaracion de una constante
+
+define("nombre","Salgado"); //valor constante por medio de la funcion define que recibe el nombre de la constante y su valor
+
+//imprimimos constante
+echo nombre;
+
+
+
+?>

f8f7a9dc148eb4d43699b36fb4b32c88dc33131d
Demian <53890358+demian35@users.noreply.github.com>
2023-06-27 21:14:17 -0600

    tipos de valores en php

---
+++ ejercicio6valores/impresionValores.php
@@ -0,0 +1,10 @@
+<?php
+
+$nombre="Jose"; //string
+$apellido="Jimenez"; //String
+$altura=1.70; //tipo decimal float
+$edad=26; //tipo entero int
+$posicion=null; //valor nula 
+
+echo "Nombre completo ".$nombre." ".$apellido." Edad: ".$edad." Altura: ".$altura." Juega de:".$posicion;
+?>

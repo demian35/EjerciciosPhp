419261601deff09271a317d9813185531692c9d3
Demian <53890358+demian35@users.noreply.github.com>
2023-06-28 23:29:59 -0600

    ejemplos de funciones de cadenas strtouper y lower

---
+++ ejercicio18funcionescadenas/ejStrtoupper.php
@@ -0,0 +1,12 @@
+<?php
+
+$texto="Hola como estas";
+
+//funcion para strings para convertir toda una cadena a minusculas
+$textomayus=strtoupper($texto);
+echo "<br>" .$textomayus;
+
+//funcion para hacer minuscula toda una cadena
+$textominus=strtolower($texto);
+echo "<br>" .$textominus;
+?>

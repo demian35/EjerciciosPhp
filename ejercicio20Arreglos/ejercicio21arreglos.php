f3d72bde14c1b6df69da4cbad6424b57962ecc94
Demian <53890358+demian35@users.noreply.github.com>
2023-07-02 22:25:48 -0600

    uso y manipulacion de arreglos con php

---
+++ ejercicio20Arreglos/ejercicio21arreglos.php
@@ -0,0 +1,8 @@
+<?php
+
+//operador => ej: "F"=> "xxxxx" sirve para asignarle el indice que nosostros queramos a algun elemento
+//del array
+$arreglo=array("S" =>"springboot", "L" =>"laravel" ,"P" =>"django","J"=>"node");
+
+print_r($arreglo);
+?>
---
+++ ejercicio20Arreglos/ejercicioarreglos.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Frameworks para desarrollo web";
+echo "<br>";
+$arreglo=array("springboot" , "laravel" , "django", "node");
+
+print_r($arreglo);
+
+?>

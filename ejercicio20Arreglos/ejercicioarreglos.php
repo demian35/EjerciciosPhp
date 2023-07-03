49452002cb82099509cbb677d32e0b71698f1f07
Demian <53890358+demian35@users.noreply.github.com>
2023-07-02 22:57:59 -0600

    acceso a un arreglo para imprimir sus elementos

--- ejercicio20Arreglos/ejercicioarreglos.php
+++ ejercicio20Arreglos/ejercicioarreglos.php
@@ -6,4 +6,10 @@ $arreglo=array("springboot" , "laravel" , "django", "node");
 
 print_r($arreglo);
 
+//acedemos a los elementos del arreglo con un ciclo for
+for($indice=0;$indice<4;$indice++){
+    echo "<br>".$arreglo[$indice]; //imprimimos el arreglo
+}
+
+
 ?>

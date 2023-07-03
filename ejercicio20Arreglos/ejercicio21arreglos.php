34b6afdd5c37b9cac8281b1c97ecdd9648ed7354
Demian <53890358+demian35@users.noreply.github.com>
2023-07-02 23:08:33 -0600

    uso de foreach para mapear elementos de un array

--- ejercicio20Arreglos/ejercicio21arreglos.php
+++ ejercicio20Arreglos/ejercicio21arreglos.php
@@ -5,4 +5,9 @@
 $arreglo=array("S" =>"springboot", "L" =>"laravel" ,"P" =>"django","J"=>"node");
 
 print_r($arreglo);
+
+//acceso a los elementos de un arreglo mediante un foreach
+foreach($arreglo as $indice =>$valor){
+    echo "<br>".$valor." Es un framework para desarrollo web y tiene el indice: ".$indice;
+}
 ?>

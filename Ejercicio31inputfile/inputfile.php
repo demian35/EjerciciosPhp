d0c2900e73cf2e4b1698c2c0bb88c3bfd7379751
Demian <53890358+demian35@users.noreply.github.com>
2023-07-07 00:09:54 -0600

    descarga de archivos con php

--- Ejercicio31inputfile/inputfile.php
+++ Ejercicio31inputfile/inputfile.php
@@ -7,8 +7,17 @@ if($_POST){
     //inprimimos informacion del archio que estamos subiendo
     print_r($_FILES["subearchivo"]["name"]);
 
-    //funcion para descargar un archivo subido previamente por medio del form html
-    move_uploaded_file($_FILES["subearchivo"]["tmp_name"],$_FILES["subearchivo"]["name"]);
+    //archivo temporal subido con el form html
+    $archivoTemp=$_FILES["subearchivo"]["tmp_name"];
+    //directorio destino donde se guardara el archivo al descargarse
+    $destino = "c:/Users/josed/OneDrive/Documentos/error.png";
+
+    //descargamos el archivo si la descarga fue exitosa se le notificara y si no marcara error
+    if(move_uploaded_file($archivoTemp,$destino)){
+        echo "descarga exitosa";
+    }else{
+        echo "error";
+    }
 
 }
 

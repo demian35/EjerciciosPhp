71098e96e6784a31100601035d6d2aa49df6fea3
Demian <53890358+demian35@users.noreply.github.com>
2023-07-07 00:53:21 -0600

    documentando codigo

--- ejercicio32lecturadejson/decodejson.php
+++ ejercicio32lecturadejson/decodejson.php
@@ -1,15 +1,18 @@
 <?php
 
+//archivo json a leer
 $ejmplojson='[
     {"nombre":"Jose", "apellido":"Salgado"},
     {"nombre":"Booker", "apellido":"Dewitt"}
 
 ]';
 
+//resultado de la decodificacion de los datos json con la funcion json_decode()
 $resultado=json_decode($ejmplojson);
-print_r($resultado);
+print_r($resultado);//imprimimos el json en forma de objeto
 echo"<br>";
 
+//imprimimos los datos del json de manera legible para el usuario
 foreach($resultado as $usuarios){
      echo $usuarios->nombre." ".$usuarios->apellido;
      echo"<br>";

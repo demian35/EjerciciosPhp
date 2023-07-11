509bbb568bf3f0904ddfc1a590a8bbe57ff493ea
Demian <53890358+demian35@users.noreply.github.com>
2023-07-10 21:09:39 -0600

    Clase principal usando include

---
+++ ejercicio35include/ejemploinclude.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Una pagina web </title>
+</head>
+<body>
+    <p>importando contenido de otro archivo con la funcion include</p>
+    <br>
+    <?php
+        include "ejemploinclude2.php";//ejecutando codigo del archivo ejemploinclude2.php con un include
+    ?>
+</body>
+</html>

c08234aaccd505cd5a9317340328178ccf0036df
Demian <53890358+demian35@users.noreply.github.com>
2023-07-10 21:39:49 -0600

    clase principal con un require()

---
+++ ejercicio36require/ejemplorequire.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Una pagina web</title>
+</head>
+<body>
+    <h1>Uso del require en php</h1>
+    <?php
+        require("ejemplorequire2.php");//require para usar lo de la clase ejemplorequire2.php
+    ?>
+</body>
+</html>
